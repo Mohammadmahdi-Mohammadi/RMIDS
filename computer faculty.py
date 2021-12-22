7bee3c3dc03e255ff82d801fbdb2d91748f4d6c7
Mohammadmahdi Mohammadi <mm.mohammadi3@gmail.com>
2021-12-23 01:55:54 +0330

    socket binding

---
+++ computer faculty.py
@@ -0,0 +1,21 @@
+
+class university:
+    def __init__(self):
+
+        self.country = "Iran"
+        self.city = "Tehran"
+        self.address = "No. 350, Hafez Ave, Valiasr Square"
+        self.name = "Amirkabir University of Technology"
+        self.contact_array =  {"tel" : "+98(21)66419506" , "fax" : "+98(21)66495519" , "email" : "pheeaut.ac.ir"}
+
+    def contact_Info(self):
+        return self.contact_array
+
+
+
+# class  faculty(university):
+#
+#     def __init__(self):
+
+    mycode = 'print ("hello world")'
+    exec(mycode)
